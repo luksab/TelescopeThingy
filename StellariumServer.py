44abb3463f705352f34d4edaa405505dde9d636d
luksab <luksab@users.noreply.github.com>
2017-09-09 16:23:43 +0200

    correct Exceptionn

--- StellariumServer.py
+++ StellariumServer.py
@@ -43,7 +43,7 @@ class SS:
                             #reply = struct.pack("3iIii", 24, 0, time.time(), data[3], data[4], 0)
                             #print repr(reply)
                             #i.send(reply)
-                        except Exceptionn as e:
+                        except Exception as e:
                             #print("StellariumServer is confused")
                             print("StellariumServer is confused:", e)
                             #self.hasStopped = True
