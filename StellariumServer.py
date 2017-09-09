9aec1d24f3f7c93c1550dcbcd98bf983408602a8
luksab <luksab@users.noreply.github.com>
2017-09-09 16:54:33 +0200

    test

--- StellariumServer.py
+++ StellariumServer.py
@@ -18,34 +18,35 @@ class SS:
     
     def run(self):
         while not self.hasStopped:
-            # Waits for I/O being available for reading from any socket object.
-            rlist, wlist, xlist = select.select( [self.listening_socket] + self.open_sockets, [], [] )
-            for i in rlist:
-                if i is self.listening_socket:
-                    new_socket, addr = self.listening_socket.accept()
-                    self.open_sockets.append(new_socket)
-                    print("NEW")
-                else:
-                    data = i.recv(1024)
-                    #print(data)
-                    if data == b'':
-                        self.open_sockets.remove(i)
-                        print("Connection closed")
+            with timeout(seconds=3):
+                # Waits for I/O being available for reading from any socket object.
+                rlist, wlist, xlist = select.select( [self.listening_socket] + self.open_sockets, [], [] )
+                for i in rlist:
+                    if i is self.listening_socket:
+                        new_socket, addr = self.listening_socket.accept()
+                        self.open_sockets.append(new_socket)
+                        print("NEW")
                     else:
-                        try:
-                            data = struct.unpack("3iIi", data)
-                            #print("%x, %o" % (data[3], data[3]))
-                            ra = data[3]*(M_PI/0x80000000)
-                            dec = data[4]*(M_PI/0x80000000)
-                            print(ra,dec)
-                            self.coord[0] = ra
-                            self.coord[1] = dec
-                            
-                            #reply = struct.pack("3iIii", 24, 0, time.time(), data[3], data[4], 0)
-                            #print repr(reply)
-                            #i.send(reply)
-                        except Exception as e:
-                            pass
-                            print("StellariumServer is confused")
-                            #print("StellariumServer is confused:", e)
-                            #self.hasStopped = True
+                        data = i.recv(1024)
+                        #print(data)
+                        if data == b'':
+                            self.open_sockets.remove(i)
+                            print("Connection closed")
+                        else:
+                            try:
+                                data = struct.unpack("3iIi", data)
+                                #print("%x, %o" % (data[3], data[3]))
+                                ra = data[3]*(M_PI/0x80000000)
+                                dec = data[4]*(M_PI/0x80000000)
+                                print(ra,dec)
+                                self.coord[0] = ra
+                                self.coord[1] = dec
+                                
+                                #reply = struct.pack("3iIii", 24, 0, time.time(), data[3], data[4], 0)
+                                #print repr(reply)
+                                #i.send(reply)
+                            except Exception as e:
+                                pass
+                                print("StellariumServer is confused")
+                                #print("StellariumServer is confused:", e)
+                                #self.hasStopped = True
