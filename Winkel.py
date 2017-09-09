9ffa63e3e94651a768b9eb7ba3cd9ba4e0e257b3
luksab <luksab@users.noreply.github.com>
2017-09-09 18:10:44 +0200

    LATLON

--- Winkel.py
+++ Winkel.py
@@ -1,4 +1,5 @@
 from sympy import *
+import datetime
 import math, time, ctypes
 libc = ctypes.CDLL('libc.so.6')
 def delay(ms):
@@ -7,10 +8,16 @@ def delay(ms):
 #def delay(ms):
 #  time.sleep(ms/1000)
 class Winkel:
-    def calc(RA, DEC, LAT, LONG, D, UT) #right ascension, declination, lattitude, longitude, days since J2000, universal time
-        ALT = math.asin(math.sin(DEC)*math.sin(LAT)+math.cos(DEC)*math.cos(LAT)*math.cos(100.46+0.985647*D+LONG+15*UT-RA))
+    def calc(RA, DEC, LAT, LONG): #right ascension, declination, lattitude, longitude, days since J2000, universal time
+        dt = datetime.now() - self.start_time
+        UT = ((dt.seconds + dt.microseconds / 1000000.0)/3600)
+        ALT = math.asin(math.sin(DEC)*math.sin(LAT)+math.cos(DEC)*math.cos(LAT)*math.cos(100.46+0.985647*self.diff+LONG+15*UT-RA))
         AZ = math.acos((math.sin(DEC)-math.sin(ALT)*math.sin(LAT))/(math.cos(ALT)*math.cos(LAT)))
-    def __init__(self,alpha = 1.5,beta = 1,StartTime = time.time(),phi = 45):
+    def __init__(self,alpha = 1.5,beta = 1,LAT=51,LON=7):
+        d = (datetime.date.today()-datetime.date(2000, 1, 1))
+        self.diff = d.days-0.5
+        d = datetime(2000,1,1)
+        self.start_time = d.replace(hour=12, minute=0)
         self.s1 = 5
         self.s2 = 5
         self.s3 = 5
@@ -19,9 +26,10 @@ class Winkel:
         self.hasStopped = False
         self.Ca = alpha
         self.Cb = beta
-        self.Cc = StartTime
-        self.Cd = phi
-    def initWithNonParallel(self,alpha = 1.5,beta = 1,StartTime = time.time(),phi = 45,gamma = 0,delta = 0):
+        self.Cc = LAT
+        self.Cd = LON
+    def initWithNonParallel(self,alpha = 1.5,beta = 1,LAT = 51,LON = 7,gamma = 0,delta = 0):
+        self.today = datetime.date.today()
         self.s1 = 5
         self.s2 = 5
         self.s3 = 5
@@ -30,11 +38,11 @@ class Winkel:
         self.hasStopped = False
         self.Ca = alpha
         self.Cb = beta
-        self.Cc = StartTime
-        self.Cd = phi
+        self.Cc = LAT
+        self.Cd = LON
         self.Ce = gamma
         self.Cf = delta
     def run(self):
         while not self.hasStopped:
-            self.calc(self.Ca,self.Cb,time.time()-self.Cc,self.Cd)
+            self.calc(self.Ca,self.Cb,self.Cc,self.Cd)
             delay(50)
